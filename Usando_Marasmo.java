2d4e136dd4a21359e884acc7f24cf7c04dad8c84
DANI <111095710+thayssaromao@users.noreply.github.com>
2024-05-14 01:54:54 -0300

    Add files via upload

---
+++ Usando_Marasmo.java
@@ -0,0 +1,14 @@
+
+public class Usando_Marasmo {
+
+   
+    public static void main(String[] args) {
+        //nome, peso ATUAL, peso USUAL
+        System.out.println(Paciente.toString("JUCA BALA",62.2,98.5));
+        System.out.println(Paciente.toString("MARIA DA SILVA",56.3,62.5));
+        System.out.println(Paciente.toString("LUCAS MOREIRA",61.35,80.75));
+        System.out.println(Paciente.toString("THAYSSA ROMAO",50,69.0));
+        System.out.println(Paciente.toString("LORENA BARRO",50,180.0));
+    }
+
+}
